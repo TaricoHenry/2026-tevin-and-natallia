9e15f514581750f41982151c21ded37849952324
ethanl2 <ethanl@enet.gy>
2026-04-04 18:12:57 -0400

    Add functionality for token, token-status and rsvp-reply API calls

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,21 @@
+const baseUrl = "https://us-central1-tevin-wedding.cloudfunctions.net:443/api/v1/"
+let params = new URLSearchParams(document.location.search);
+const token = params.get("token");
+
+window.onload = startLoad();
+window.onload = checkTokenStatus();
+
+function startLoad() {
+  show = setTimeout(showPage, 1000);
+}
+
+function showPage() {
+  document.getElementsByClassName("welcome")[0].style.display = "flex";
+  document.getElementsByClassName("story")[0].style.display = "flex";
+  document.getElementsByClassName("invitation")[0].style.display = "flex";
+}
+
+
 var countDownDate = new Date("Jul 25, 2026 14:00:00").getTime();
 var x = setInterval(function() {
 
@@ -39,76 +57,85 @@ animElements.forEach(el => observer.observe(el))
 
 //END fade in code
 
-async function checkToken() {
-    let params = new URLSearchParams(document.location.search);
-    let token = params.get("token");
-    console.log(token);
-    var url = ('https://us-central1-tevin-wedding.cloudfunctions.net:443/api/v1/token/'+token+'/status');
+//START Check Token Status
+
+async function checkTokenStatus() {
+    if (token == null) {
+        document.getElementById("submit").style.display = "none";     
+        return;
+    }
+    var url = (baseUrl + 'token/' + token + '/status');
+    //baseUrl and token declared at the top
 
     try {
-        const response = await fetch(url, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                }
-            })
-        console.log(response)
+    const response = await fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        }
+    })
+    let tokenStatusBody = await response.json();
+        if (tokenStatusBody.valid == false) {
+            document.getElementById("submit").style.display = "none";     
+        }
+        else if (tokenStatusBody.used == false && tokenStatusBody.valid == true) {
+            populateTokenData();
+        }
+
     } catch (error) {
         console.error(error.message);
     }
 }
 
-const sampleResponse = {
-    "isSuccess": true,
-    "token": "AB12CD",
-    "household": "The Smith Family",
-    "householdSize": 2,
-    "allResponded": false,
-    "members": [
-        {
-            "memberId":"abc123",
-            "name":"personA Smith",
-            "personalizedAddy":"Mr personA",
-            "rsvp":false,
-            "respondedAt":"11:59"
-        },
-        {
-            "memberId":"abc124",
-            "name":"personB Smith",
-            "personalizedAddy":"Mr personB",
-            "rsvp":false,
-            "respondedAt":"11:59"
+//END Check Token Status
+
+
+//START Populate Token Code
+
+async function populateTokenData() {
+    var url = (baseUrl + 'token/' + token);
+    //baseUrl and token declared at the top
+
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+        let tokenDetailsBody = await response.json();
+        var guestBoxes = "";
+
+        for ( i of tokenDetailsBody.members) {
+            guestBoxes = guestBoxes + 
+            `            
+                <div class="guest-box">
+                    <label for="${i.memberId}">
+                        ${i.personalizedAddy}
+                    </label>
+                    <input id="${i.memberId}" class="guest-checkbox" type="checkbox"/>
+                </div>
+            `
         }
-        
-    ]
+
+        document.getElementsByClassName("rsvp")[0].innerHTML =
+            `
+            <p>Kindly let us know who will be attending:</p>
+            </br>
+            </br>
+            ${guestBoxes}
+            <input id="submit" type="button" value="Submit" onclick="submitResponse()">
+            `;
+
+        document.getElementsByClassName("household")[0].innerHTML = tokenDetailsBody.household;
+
+    } catch (error) {
+        console.error(error.message);
+    }
 }
 
-//START Parse JSON Code
 
-var guestBoxes = ""
-
-for ( i of sampleResponse.members) {
-    guestBoxes = guestBoxes + 
-    `            
-        <div class="guest-box">
-            <label for="${i.memberId}">
-                ${i.personalizedAddy}
-            </label>
-            <input id="${i.memberId}" class="guest-checkbox" type="checkbox"/>
-        </div>
-    `
-}
-
-document.getElementsByClassName("rsvp")[0].innerHTML =
-    `
-    <p>Kindly let us know who will be attending:</p>
-    </br>
-    </br>
-    ${guestBoxes}
-    <input id="submit" type="button" value="Submit" onclick="checkToken()">
-    `;
-
-//END Parse JSON Code
+//END Populate Token Code
 
 //START checkbox code
 
@@ -120,3 +147,37 @@ for (i of checkboxes) {
 }
 
 //END checkbox code
+
+//START Submit Code
+
+async function submitResponse() {
+    document.getElementById("submit").value = "Submitting...";
+    boxes =  Array.from(document.getElementsByClassName("guest-checkbox"))
+
+    const submissionArray = boxes.map(guest => ({
+        memberId: guest.id,
+        rsvp: guest.checked == true ? "yes" : "no"
+    }))
+
+    try {
+        let url = baseUrl + 'token/' + token + '/reply'
+        //baseUrl and token declared at the top
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ responses: submissionArray })
+        });
+        const result = await response.json();
+        console.log(result);
+        setTimeout(() => {
+            window.location.replace("/thank-you");
+        }, 1500);
+
+    }
+    catch (error) {
+        console.error(error.message);
+    }
+}
+//END Submit Code
